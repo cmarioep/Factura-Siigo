6282d21ea5329ed1753c551dbd029411b1a23d89
cmarioep <cmescandonpardo@gmail.com>
2023-04-20 09:07:56 -0500

    Feature: getCurrentDate function, Siigo date format

---
+++ src/utils/getCurrentDate.js
@@ -0,0 +1,9 @@
+function getCurrentDate() {
+    const today = new Date();
+    const year = today.getFullYear();
+    const month = (today.getMonth() + 1).toString().padStart(2, '0');
+    const day = today.getDate().toString().padStart(2, '0');
+    return `${year}-${month}-${day}`;
+  }
+
+console.log(getCurrentDate());
