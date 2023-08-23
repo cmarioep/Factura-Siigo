9b1cffd3be2f0bd40aed9f550dcdc1df88cff1cf
cmarioep <cmarioescandonpardo@gmail.com>
2023-08-23 10:17:11 -0500

    Data: JSON Customers

---
+++ src/data/jsondata.js
@@ -0,0 +1,173 @@
+[
+    {
+        "person_type": "Company",
+        "id_type": "31",
+        "identification": "901487064",
+        "name": ["GESTION LOGISTICA INTEGRAL S.A.S"],
+        "address": {
+            "address": "AV 6 OESTE 32 25",
+            "city": {
+                "country_code": "Co",
+                "state_code": "76",
+                "city_code": "76001"
+            }
+        },
+        "phones": [
+            {
+                "number": "3225939366"
+            }
+        ],
+        "contacts": [
+            {
+                "first_name": "LINA VIVIANA",
+                "last_name": "QUINTERO FRANCO",
+                "email": "gestionlogisticaintegralsas@gmail.com"
+            }
+        ]
+    },
+    {
+        "person_type": "Company",
+        "id_type": "31",
+        "identification": "901222624",
+        "name": ["ASESORIAS Y COMERCIALIZADORA DEL VALLE S.A.S."],
+        "address": {
+            "address": "AV ESTACION 5 A N 27",
+            "city": {
+                "country_code": "Co",
+                "state_code": "76",
+                "city_code": "76001"
+            }
+        },
+        "phones": [
+            {
+                "number": "3185406572"
+            }
+        ],
+        "contacts": [
+            {
+                "first_name": "ROSALINA",
+                "last_name": "FRANCO BASTIDAS",
+                "email": "asesoriadelvalle2020@gmail.com"
+            }
+        ]
+    }
+]
+
+
+
+
+
+// **********************
+// Registros repetidos
+// **********************
+
+[
+    {
+        "person_type": "Company",
+        "id_type": "31",
+        "identification": "901338854",
+        "name": ["GRUPO CREATIVO AV SAS"],
+        "address": {
+            "address": "C10 MZ R CA 14",
+            "city": {
+                "country_code": "Co",
+                "state_code": "76",
+                "city_code": "76001"
+            }
+        },
+        "phones": [
+            {
+                "number": "5555555"
+            }
+        ],
+        "contacts": [
+            {
+                "first_name": "JOSE OMAR",
+                "last_name": "ZAPATA ARCE",
+                "email": "comercianteibg@gmail.com"
+            }
+        ]
+    },
+    {
+        "person_type": "Company",
+        "id_type": "31",
+        "identification": "901338328",
+        "name": ["TOLIGROUP INNOVACION SAS"],
+        "address": {
+            "address": "MZ B CA 41",
+            "city": {
+                "country_code": "Co",
+                "state_code": "73",
+                "city_code": "73001"
+            }
+        },
+        "phones": [
+            {
+                "number": "3103260642"
+            }
+        ],
+        "contacts": [
+            {
+                "first_name": "LINA TATIANA",
+                "last_name": "VILLEGAS RODRIGUEZ",
+                "email": "ibague3efectivos@gmail.com"
+            }
+        ]
+    },
+    {
+        "person_type": "Company",
+        "id_type": "31",
+        "identification": "901336464",
+        "name": ["GRUPINS SAS"],
+        "address": {
+            "address": "TO 2 AP 904 ALTAGRACIA",
+            "city": {
+                "country_code": "Co",
+                "state_code": "73",
+                "city_code": "73001"
+            }
+        },
+        "phones": [
+            {
+                "number": "5555555"
+            }
+        ],
+        "contacts": [
+            {
+                "first_name": "JOSE SAUL",
+                "last_name": "RODRIGUEZ VANEGAS",
+                "email": "ibague5efectivos@gmail.com"
+            }
+        ]
+    },
+    {
+        "person_type": "Company",
+        "id_type": "31",
+        "identification": "901336706",
+        "name": ["INNOVANDO SERVICE SAS"],
+        "address": {
+            "address": "MZ C CS 49",
+            "city": {
+                "country_code": "Co",
+                "state_code": "73",
+                "city_code": "73001"
+            }
+        },
+        "phones": [
+            {
+                "number": "5555555"
+            }
+        ],
+        "contacts": [
+            {
+                "first_name": "CLAUDIA EMILSEN",
+                "last_name": "RODRIGUEZ ARCE",
+                "email": "innovandoservice@gmail.com"
+            }
+        ]
+    }
+]
+
+
+
+
